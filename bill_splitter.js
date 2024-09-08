fb689a608a4896dc65de8c3f233d8dbb6e1ef33f
JovMan <174271476+JovTheMan@users.noreply.github.com>
2024-09-08 10:15:18 -0400

    Add output functionality to display bill, tip, and total

--- bill_splitter.js
+++ bill_splitter.js
@@ -1,6 +1,11 @@
-// Step 1: Ternarary Operator Logic 
+// Step 2: Display Output Details (Check first commit for Step 1)
 
 let bill = 275; // Example bill value
 let tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;
+let total = bill + tip;
 
-console.log(tip);
+console.log(`The bill was $${bill}, the tip was $${tip}, and the total value is $${total}`);
+
+
+
+
